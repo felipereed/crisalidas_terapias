3be1f56c2f40fd1cfe41701f5c40c7ac2555a32f
Felipe Lins <contactfelipelins@gmail.com>
2022-10-01 15:51:36 -0400

    mobile homepage started

--- crisalidas-app/src/App.js
+++ crisalidas-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './header/header';
 import Cards from './cards/cards';
 import HomepageImage from './homepage-image/homepage-image';
+import Button from './common/button';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <HomepageImage></HomepageImage>
+      <img className='mobile-background' src="./mobile_background.jpg" alt="" />
+        <p className='app-call-to-action-text'>Psicologia clínica de abordagem Junguiana, Terapia Floral e Integrativa.</p>
+        <Button></Button>
       <Cards></Cards>
     </div>
   );
